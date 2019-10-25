68a78672800e1a5a88685c6c2a6e9a12836f4b13
Mogens Heller Grabe <mookid8000@gmail.com>
2019-10-25 20:44:40 +0200

    update testy and whatnot

--- Topos.Tests.Contracts/Broker/BatchProducerTest.cs
+++ Topos.Tests.Contracts/Broker/BatchProducerTest.cs
@@ -2,8 +2,13 @@
 using System.Linq;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Testy.Benchmarking;
+using Testy.Extensions;
+using Topos.Config;
 using Topos.Producer;
 using Topos.Tests.Contracts.Factories;
+// ReSharper disable ArgumentsStyleAnonymousFunction
+#pragma warning disable 1998
 
 namespace Topos.Tests.Contracts.Broker
 {
@@ -15,10 +20,10 @@ namespace Topos.Tests.Contracts.Broker
 
         [TestCase(10, true)]
         [TestCase(10, false)]
-        [Ignore("not ready yet")]
         public async Task ProduceEvents(int eventCount, bool useBatchApi)
         {
-            var events = Enumerable.Range(0, eventCount).Select(n => $"THIS IS EVENT NUMBER {n}");
+            var events = Enumerable.Range(0, eventCount)
+                .Select(n => new ToposMessage($"THIS IS EVENT NUMBER {n}"));
 
             var topic = _factory.GetNewTopic();
 
@@ -28,16 +33,19 @@ namespace Topos.Tests.Contracts.Broker
                     .Create()
             );
 
-            if (!useBatchApi)
+            using (new TimerScope($"Send {eventCount} messages", countForRateCalculation: eventCount))
             {
-                foreach (var e in events)
+                if (!useBatchApi)
                 {
-                    await producer.Send(new ToposMessage(e), partitionKey: "whatever");
+                    foreach (var evt in events)
+                    {
+                        await producer.Send(evt, partitionKey: "whatever");
+                    }
                 }
-            }
-            else
-            {
-                //await producer.SendMany(events.Select(e => new ToposMessage(e)), partitionKey: "whatever");
+                else
+                {
+                    await producer.SendMany(events, partitionKey: "whatever");
+                }
             }
 
             var queue = new ConcurrentQueue<string>();
@@ -45,9 +53,21 @@ namespace Topos.Tests.Contracts.Broker
             var consumer = Using(
                 _factory
                     .ConfigureConsumer("default")
-
+                    .Topics(t => t.Subscribe(topic))
+                    .Handle(async (messages, context, cancellationToken) =>
+                    {
+                        queue.EnqueueRange(messages.Select(m => m.Body).Cast<string>());
+                    })
+                    .Positions(p => p.StoreInMemory())
                     .Create()
             );
+
+            consumer.Start();
+
+            using (new TimerScope($"Receive {eventCount} messages", countForRateCalculation: eventCount))
+            {
+                await queue.WaitOrDie(q => q.Count == eventCount, failExpression: q => q.Count > eventCount);
+            }
         }
     }
 }
--- Topos/Producer/IToposProducer.cs
+++ Topos/Producer/IToposProducer.cs
@@ -23,18 +23,18 @@ namespace Topos.Producer
         /// </param>
         Task Send(ToposMessage message, string partitionKey = null);
 
-        ///// <summary>
-        ///// Sends the given messages.
-        ///// </summary>
-        ///// <param name="messages">
-        ///// The messages to send. The messages are always a sequence of <see cref="ToposMessage"/>.
-        ///// Each <see cref="ToposMessage.Body"/> will be serialized using the configured serializer, and the topic will be
-        ///// determined by the current topic mapper. An optional dictionary of <see cref="ToposMessage.Headers"/> can be added, too.
-        ///// </param>
-        ///// <param name="partitionKey">
-        ///// Optional parameter that will be used to determine which partition the sent messages will be sent to.
-        ///// Remember that if the order of the messages is important, you must specify a partition key that ensures partition affinity.
-        ///// </param>
-        //Task SendMany(IEnumerable<ToposMessage> messages, string partitionKey = null);
+        /// <summary>
+        /// Sends the given messages.
+        /// </summary>
+        /// <param name="messages">
+        /// The messages to send. The messages are always a sequence of <see cref="ToposMessage"/>.
+        /// Each <see cref="ToposMessage.Body"/> will be serialized using the configured serializer, and the topic will be
+        /// determined by the current topic mapper. An optional dictionary of <see cref="ToposMessage.Headers"/> can be added, too.
+        /// </param>
+        /// <param name="partitionKey">
+        /// Optional parameter that will be used to determine which partition the sent messages will be sent to.
+        /// Remember that if the order of the messages is important, you must specify a partition key that ensures partition affinity.
+        /// </param>
+        Task SendMany(IEnumerable<ToposMessage> messages, string partitionKey = null);
     }
 }
