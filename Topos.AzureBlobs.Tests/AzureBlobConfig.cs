b2a57aae8f9564589e608e75ef5b34bc23e8d046
Mogens Heller Grabe <mhg@mhg-mba1.local>
2021-01-07 22:00:03 +0100

    change to net5 and stuff

--- Topos.AzureBlobs.Tests/AzureBlobConfig.cs
+++ Topos.AzureBlobs.Tests/AzureBlobConfig.cs
@@ -6,40 +6,6 @@ namespace Topos.AzureBlobs.Tests
 {
     static class AzureBlobConfig
     {
-        public static CloudStorageAccount StorageAccount
-        {
-            get
-            {
-                return CloudStorageAccount.DevelopmentStorageAccount;
-
-                var filePath = Path.Combine(AppContext.BaseDirectory, "azure_storage_account_connection_string.secret.txt");
-
-                if (File.Exists(filePath)) return CloudStorageAccount.Parse(File.ReadAllText(filePath));
-
-                const string varName = "azure_storage_account_connection_string";
-                var environmentVariable = Environment.GetEnvironmentVariable(varName);
-
-                if (!string.IsNullOrWhiteSpace(environmentVariable))
-                {
-                    return CloudStorageAccount.Parse(environmentVariable);
-                }
-
-                throw new ApplicationException($@"Could not get Azure storage account connection string!
-
-Tried loading it from the file
-
-    {filePath}
-
-but the file does not exist.
-
-Also tried looking at the ENV variable named
-
-    {varName}
-
-but it was empty.
-
-Please provide a connection string through one of these for the tests to run.");
-            }
-        }
+        public static CloudStorageAccount StorageAccount => CloudStorageAccount.DevelopmentStorageAccount;
     }
 }
