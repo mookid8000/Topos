114518127b624c007e17710bbb50478f72db25cb
Rune Nielsen <runenielsen@runbox.com>
2021-06-29 16:17:43 +0200

    adds postgresql position manager

---
+++ Topos.PostgreSql.Tests/PostgreSqlFixtureBase.cs
@@ -0,0 +1,9 @@
+using Topos.Tests;
+
+namespace Topos.PostgreSql.Tests
+{
+    public abstract class PostgreSqlFixtureBase : ToposFixtureBase
+    {
+        protected void CleanDatabase(string connectionString) => PostgreSqlTestConfig.CleanDatabase(connectionString);
+    }
+}
