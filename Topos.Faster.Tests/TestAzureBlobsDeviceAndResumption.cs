d56324354fbdd69b2171908b85ca3df8d7f0e260
Mogens Heller Grabe <mookid8000@gmail.com>
2023-04-24 23:16:51 +0200

    test cases for reuse/no reuse

--- Topos.Faster.Tests/TestAzureBlobsDeviceAndResumption.cs
+++ Topos.Faster.Tests/TestAzureBlobsDeviceAndResumption.cs
@@ -11,6 +11,7 @@ using Topos.Consumer;
 using Topos.Faster.Tests.Factories;
 using Topos.InMem;
 using Topos.Logging.Console;
+using Topos.Producer;
 
 #pragma warning disable CS1998
 
@@ -35,31 +36,34 @@ public class TestAzureBlobsDeviceAndResumption : FixtureBase
     [TestCase(10)]
     [TestCase(30)]
     [TestCase(100)]
-    [TestCase(1000)]
-    [TestCase(10091)]
-    public async Task CanResumeAfterRestarting(int numberOfRestarts)
+    public async Task CanResumeAfterRestarting_NoReuse(int numberOfRestarts)
     {
-        var connectionString = BlobStorageDeviceManagerFactory.StorageConnectionString;
-
+        const string connectionString = BlobStorageDeviceManagerFactory.StorageConnectionString;
+        const string directoryName = "directory";
+        
+        // the only thing that survives between iterations in the in-mem positions storage
         var positions = new InMemPositionsStorage();
+
+        // reduce amount of logging if running many iterations
         var logLevel = numberOfRestarts < 10 ? LogLevel.Debug : LogLevel.Info;
 
-        using var producer = Configure
-            .Producer(s => s.UseAzureStorage(connectionString, _containerName, "directory"))
+        var receivedMessages = new ConcurrentQueue<string>();
+
+        IToposProducer CreateProducer() => Configure
+            .Producer(s => s.UseAzureStorage(connectionString, _containerName, directoryName))
             .Logging(l => l.UseConsole(logLevel))
             .Serialization(s => s.UseNewtonsoftJson())
             .Create();
 
-        var receivedMessages = new ConcurrentQueue<string>();
-
-        IDisposable StartConsumer() => Configure
-            .Consumer("default", s => s.UseAzureStorage(connectionString, _containerName, "directory"))
+        IDisposable CreateConsumer() => Configure
+            .Consumer("default", s => s.UseAzureStorage(connectionString, _containerName, directoryName))
             .Logging(l => l.UseConsole(logLevel))
             .Serialization(s => s.UseNewtonsoftJson())
             .Positions(p =>
             {
                 p.StoreInMemory(positions);
 
+                // decorate positions manager to log positions whenever they're loaded/saved
                 StandardConfigurer.Open(p)
                     .Decorate(c => new ConsoleLoggingPositionsManagerDecorator(c.Get<IPositionManager>()));
             })
@@ -69,12 +73,67 @@ public class TestAzureBlobsDeviceAndResumption : FixtureBase
 
         for (var counter = 1; counter <= numberOfRestarts; counter++)
         {
-            using (StartConsumer())
-            {
-                await producer.Send("topic1", new($"besked {counter}"));
+            using var consumer = CreateConsumer();
+            using var producer = CreateProducer();
 
-                await receivedMessages.WaitOrDie(q => q.Count == counter, failExpression: q => q.Count > counter);
-            }
+            await producer.Send("topic1", new($"besked {counter}"));
+
+            await receivedMessages.WaitOrDie(q => q.Count == counter, failExpression: q => q.Count > counter);
+        }
+
+        Assert.That(receivedMessages, Is.EqualTo(Enumerable.Range(1, numberOfRestarts).Select(n => $"besked {n}")));
+    }
+
+    [TestCase(2)]
+    [TestCase(3)]
+    [TestCase(10)]
+    [TestCase(30)]
+    [TestCase(100)]
+    [TestCase(1000)]
+    public async Task CanResumeAfterRestarting_ReuseProducer(int numberOfRestarts)
+    {
+        const string connectionString = BlobStorageDeviceManagerFactory.StorageConnectionString;
+        const string directoryName = "directory";
+        
+        // the only thing that survives between iterations in the in-mem positions storage
+        var positions = new InMemPositionsStorage();
+
+        // reduce amount of logging if running many iterations
+        var logLevel = numberOfRestarts < 10 ? LogLevel.Debug : LogLevel.Info;
+
+        var receivedMessages = new ConcurrentQueue<string>();
+
+        IToposProducer CreateProducer() => Configure
+            .Producer(s => s.UseAzureStorage(connectionString, _containerName, directoryName))
+            .Logging(l => l.UseConsole(logLevel))
+            .Serialization(s => s.UseNewtonsoftJson())
+            .Create();
+
+        IDisposable CreateConsumer() => Configure
+            .Consumer("default", s => s.UseAzureStorage(connectionString, _containerName, directoryName))
+            .Logging(l => l.UseConsole(logLevel))
+            .Serialization(s => s.UseNewtonsoftJson())
+            .Positions(p =>
+            {
+                p.StoreInMemory(positions);
+
+                // decorate positions manager to log positions whenever they're loaded/saved
+                StandardConfigurer.Open(p)
+                    .Decorate(c => new ConsoleLoggingPositionsManagerDecorator(c.Get<IPositionManager>()));
+            })
+            .Topics(t => t.Subscribe("topic1"))
+            .Handle(async (messages, _, _) => receivedMessages.EnqueueRange(messages.Select(m => m.Body).OfType<string>()))
+            .Start();
+
+        using var producer = CreateProducer();
+
+        for (var counter = 1; counter <= numberOfRestarts; counter++)
+        {
+            using var consumer = CreateConsumer();
+
+            await producer.Send("topic1", new($"besked {counter}"));
+
+            await receivedMessages.WaitOrDie(q => q.Count == counter, failExpression: q => q.Count > counter);
         }
 
         Assert.That(receivedMessages, Is.EqualTo(Enumerable.Range(1, numberOfRestarts).Select(n => $"besked {n}")));
